02540beb6d50ca9a1a4f083072c8f1bb5c9e3b44
user <user@example.com>
2024-06-11 10:21:52 +0300

    minor fixes

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,3 @@
-import serializers
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
